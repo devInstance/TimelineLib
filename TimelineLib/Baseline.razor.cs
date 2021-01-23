f87d57cb70f2e5826811a5a5337cb0c902467228
yfranz <yfranz@gmail.com>
2021-01-22 16:48:01 -0800

    Refactoring: moving common code to the baseline component

---
+++ TimelineLib/Baseline.razor.cs
@@ -0,0 +1,94 @@
+﻿using DevInstance.LogScope;
+using DevInstance.TimelineLib.Model;
+using DevInstance.TimelineLib.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevInstance.TimelineLib
+{
+    public partial class Baseline
+    {
+        private DateTime currentDateTime = DateTime.Now;
+
+        private TimeRange timeRange;
+        private float cellWidthPercent;
+
+        private TimeScaleLabelItem[] TimeScaleLabel;
+        private TimeScaleItem[] TimeScale;
+
+        private ILog log;
+
+        protected override void OnInitialized()
+        {
+            log = LogProvider.CreateLogger(this);
+            using (var l = log.DebugExScope())
+            {
+                timeRange = TimeRangeCalculator.CreateTimeRange(StartTime, EndTime);
+                cellWidthPercent = 100.0f / (float)timeRange.Span;
+
+                InitializeTimeScale();
+                StateHasChanged();
+            }
+        }
+
+        private void InitializeTimeScale()
+        {
+            var now = DateTime.Today;
+            using (var l = log.DebugExScope())
+            {
+                var labels = new List<TimeScaleLabelItem>();
+                for (int time = (int)timeRange.StartTime; time <= timeRange.EndTime; time += 1)
+                {
+                    var item = new TimeScaleLabelItem();
+
+                    item.cssClass = time % 2 == 1 ? "odd-label" : "even-label";
+                    item.labelText = now.AddHours(time).ToString("hhtt").ToLower();
+
+                    if (time == timeRange.StartTime || time == timeRange.EndTime - 1)
+                    {
+                        item.divStyle = $"max-width:{cellWidthPercent - 1}%;width:{cellWidthPercent - 1}%";
+                    }
+                    else if (time == timeRange.EndTime)
+                    {
+                        item.divStyle = $"max-width:1%;width:1%";
+                    }
+                    else
+                    {
+                        item.divStyle = $"max-width:{cellWidthPercent}%;width:{cellWidthPercent}%";
+                    }
+                    labels.Add(item);
+                }
+                TimeScaleLabel = labels.ToArray();
+
+                var items = new List<TimeScaleItem>();
+                for (int i = 0, count = (int)timeRange.Span; i < count; i++)
+                {
+                    var item = new TimeScaleItem
+                    {
+                        lineonex = $"{i * cellWidthPercent}%",
+                        lineoney = "16",
+                        linetwox = $"{i * cellWidthPercent + cellWidthPercent / 2}%",
+                        linetwoy = "8"
+                    };
+                    items.Add(item);
+                }
+
+                TimeScale = items.ToArray();
+            }
+        }
+
+        protected override bool ShouldRender()
+        {
+            using (var l = log.DebugExScope())
+            {
+                InitializeTimeScale();
+
+                return true;
+            }
+        }
+
+    }
+}
--- TimelineLib/Heatline.razor.cs
+++ TimelineLib/Heatline.razor.cs
@@ -11,13 +11,9 @@ namespace DevInstance.TimelineLib
 {
     public partial class Heatline
     {
-        private DateTime currentDateTime = DateTime.Now;
-
         private TimeRange timeRange;
         private float cellWidthPercent;
 
-        private TimeScaleLabelItem[] TimeScaleLabel;
-        private TimeScaleItem[] TimeScale;
         private HeatItem[] HeatItems;
 
         private int svgHeight;
@@ -55,45 +51,6 @@ namespace DevInstance.TimelineLib
             using (var l = log.DebugExScope())
             {
                 CalculateTimeRange();
-
-                var labels = new List<TimeScaleLabelItem>();
-                for (int time = (int)timeRange.StartTime; time <= timeRange.EndTime; time += 1)
-                {
-                    var item = new TimeScaleLabelItem();
-
-                    item.cssClass = time % 2 == 1 ? "odd-label" : "even-label";
-                    item.labelText = now.AddHours(time).ToString("hhtt").ToLower();
-
-                    if (time == timeRange.StartTime || time == timeRange.EndTime - 1)
-                    {
-                        item.divStyle = $"max-width:{cellWidthPercent - 1}%;width:{cellWidthPercent - 1}%";
-                    }
-                    else if (time == timeRange.EndTime)
-                    {
-                        item.divStyle = $"max-width:1%;width:1%";
-                    }
-                    else
-                    {
-                        item.divStyle = $"max-width:{cellWidthPercent}%;width:{cellWidthPercent}%";
-                    }
-                    labels.Add(item);
-                }
-                TimeScaleLabel = labels.ToArray();
-
-                var items = new List<TimeScaleItem>();
-                for (int i = 0, count = (int)timeRange.Span; i < count; i++)
-                {
-                    var item = new TimeScaleItem
-                    {
-                        lineonex = $"{i * cellWidthPercent}%",
-                        lineoney = "16",
-                        linetwox = $"{i * cellWidthPercent + cellWidthPercent / 2}%",
-                        linetwoy = "8"
-                    };
-                    items.Add(item);
-                }
-
-                TimeScale = items.ToArray();
 
                 if (Data != null)
                 {
